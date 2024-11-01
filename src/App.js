2fd4090698165fb175e548e6365cce874e3a5fd0
Jackson Wozniak <jjw30@comcast.net>
2024-11-01 14:17:20 -0400

    working investment calculator

--- src/App.js
+++ src/App.js
@@ -1,9 +1,70 @@
 import './styles/App.css';
+import {useState} from 'react';
+import InvestmentChart from './components/InvestmentChart';
 
 function App() {
+
+  const [startingAmount, setStartingAmount] = useState(0);
+  const [interestRate, setInterestRate] = useState(0);
+  const [investmentDuration, setInvestmentDuration] = useState(0);
+  const [annualContributions, setAnnualContributions] = useState(0);
+  const [finalAmount, setFinalAmount] = useState(0);
+  const [data, setData] = useState([0]);
+  const [labels, setLabels] = useState([0]);
+
+  const newData = () => {
+    let updatedData = [];
+    let updatedLabels = [];
+    let finalAmount = parseFloat(startingAmount);
+    updatedLabels.push(0);
+    updatedData.push(finalAmount);
+    for(let i = 0; i < investmentDuration; i++){
+        updatedLabels.push("Year " + (i + 1));
+        let interestAmount = (parseFloat(interestRate) / 100.00) * finalAmount;
+        finalAmount += parseFloat(interestAmount);
+        console.log(finalAmount);
+        finalAmount += parseFloat(annualContributions);
+        updatedData.push(finalAmount);
+    }
+
+    setFinalAmount(parseFloat(finalAmount));
+    setData(updatedData);
+    setLabels(updatedLabels);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    newData();
+  }
+
   return (
-    <div className="App">
-      
+    <div className="container">
+      <div className="input">
+        <form onClick={handleSubmit} className="input-form">
+          <div>
+            <label>Starting Amount
+              <input type="number" value={startingAmount} onChange={(e) => setStartingAmount(e.target.value) || 0}/>
+              </label>
+              <br />
+              <label>Annual Interest Rate (%)
+                <input type="number" value={interestRate} onChange={(e) => setInterestRate(e.target.value) || 0}/>
+              </label>
+              <br />
+              <label>Investment Duration (years)
+                <input type="number" value={investmentDuration} onChange={(e) => setInvestmentDuration(e.target.value) || 0}/>
+              </label>
+              <br />
+              <label>Annual Contributions
+                <input type="number" value={annualContributions} onChange={(e) => setAnnualContributions(e.target.value) || 0}/>
+              </label>
+              <br />
+            </div>
+            <button type="submit">New Chart</button>
+        </form>
+      </div>  
+      <div className="output">
+        <InvestmentChart data={data} labels={labels} finalAmount={finalAmount} className="chart-window"></InvestmentChart>
+      </div>
     </div>
   );
 }
---
+++ src/components/InvestmentChart.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Chart from "chart.js/auto";
+import {Line} from "react-chartjs-2";
+
+function InvestmentChart(props){
+
+    const formatter = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2
+    });
+
+    const data = {
+        labels: props.labels,
+        datasets: [{
+            data: props.data,
+            fill : true,
+            backgroundColor: 'rgba(59, 209, 111, .1)',
+            borderColor: 'rgba(59, 209, 111)',
+            borderWidth: 1
+        }],
+    };
+
+    const options = {
+        plugins: {
+            title: {
+            display: true,
+            text: 'Portfolio Growth',
+            color : 'rgba(59, 209, 111)',
+            font: {
+                size: 30
+            }
+        },
+        legend: {
+            display: false
+        },
+        subtitle: {
+            display: true,
+            text: "Final Value: " + formatter.format(props.finalAmount),
+            color : 'rgba(255,255,255)',
+            font : {
+                size : 20
+            }
+        }
+        }
+    }
+
+    return(
+        <div className="chart-container">
+            <Line data={data} options={options}/>
+        </div>
+    );
+}
+
+export default InvestmentChart;
